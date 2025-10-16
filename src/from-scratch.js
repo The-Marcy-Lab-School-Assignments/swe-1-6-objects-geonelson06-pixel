19d4bd7b289b3fcc6e1413de9a699723fd678893
Gabriel <geonelson06@gmail.com>
2025-10-16 01:06:29 -0400

    Completed 1-6

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,31 +1,74 @@
-const coolGreeting = () => {
+const coolGreeting = (person) => {
+  if (person.isCool) {
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`;
+  } else {
+    return `Greetings ${person.name}, how have you been lately?`;
+  }
+
 };
 
-const haveBirthday = () => {
+const haveBirthday = (person) => {
+  person.age += 1;
+
 };
 
-const becomeSecretAgent = () => {
+const becomeSecretAgent = (person, spyHandle) => {
+  delete person.name;
+  person.name = spyHandle;
+
 };
 
-const carMaker = () => {
+const carMaker = (name, maker, year) => {
+  return {
+    name: name,
+    maker: maker,
+    year: year,
+    needsOilChange: false
+  };
+
 };
 
-const weAreNotFriends = () => {
+const weAreNotFriends = (person) => {
+  return person.friends.pop();
+
 };
 
-const listHobbies = () => {
+const listHobbies = (person) => {
+  for (let hobby of person.hobbies) {
+    console.log(`${person.name} likes ${hobby}.`);
+  }
+
 };
 
-const getNextOpponent = () => {
+const getNextOpponent = (team) => {
+  if (team.matches.length === 0) return null;
+  return team.matches[0].teamName;
+
 };
 
-const listAllKeys = () => {
+const listAllKeys = (obj) => {
+  return Object.keys(obj);
+
 };
 
-const listAllValues = () => {
+const listAllValues = (obj) => {
+  return Object.values(obj);
+
 };
 
-const convertToMatrix = () => {
+const convertToMatrix = (arrOfObjects) => {
+  if (arrOfObjects.length === 0) return [];
+
+  const keys = Object.keys(arrOfObjects[0]);
+  const matrix = [keys];
+
+  for (let obj of arrOfObjects) {
+    const values = keys.map(key => obj[key]);
+    matrix.push(values);
+  }
+
+  return matrix;
+
 };
 
 module.exports = {
